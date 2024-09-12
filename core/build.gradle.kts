a110a59b70a90c6f968399f0983595812ad16be8
dlswns2480 <dlswns661035@gmail.com>
2024-09-12 16:06:50 +0900

    docs : jwt 의존성

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -24,6 +24,11 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
+    //jwt
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
+
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mysql")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
