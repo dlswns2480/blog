7478cddb0826ecad3e0870871fa392718ddad28d
dlswns2480 <dlswns661035@gmail.com>
2024-09-13 13:09:02 +0900

    feat : UserErrorCode

--- core/src/main/kotlin/com/junlog/user/exception/UserErrorCode.kt
+++ core/src/main/kotlin/com/junlog/user/exception/UserErrorCode.kt
@@ -6,5 +6,7 @@ enum class UserErrorCode(
     override val message: String,
     override val code: String
 ) : ErrorCode{
-    NOT_FOUND_USER("존재하지 않는 유저입니다.", "U_001")
+    NOT_FOUND_USER("존재하지 않는 유저입니다.", "U_001"),
+    WRONG_PASSWORD("비밀번호가 올바르지 않습니다", "U_002"),
+    ALREADY_EXISTS_NAME("이미 존재하는 이름(닉네임)입니다.", "U_003")
 }
