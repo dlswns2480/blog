77147830270acd9fa706f2a361b14e991d31b772
dlswns2480 <dlswns661035@gmail.com>
2024-09-13 13:06:14 +0900

    feat : 회원가입, 로그인 api 구현

---
+++ api/src/main/kotlin/com/junlog/user/UserController.kt
@@ -0,0 +1,40 @@
+package com.junlog.user
+
+import com.junlog.auth.domain.model.Token
+import com.junlog.common.wrapper.ResponseWrapper.wrapOk
+import com.junlog.user.application.UserService
+import com.junlog.user.dto.request.SignInRequest
+import com.junlog.user.dto.request.SignUpRequest
+import com.junlog.user.dto.request.toDto
+import com.junlog.user.dto.response.UserResponse
+import com.junlog.user.dto.response.toResponse
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/users")
+class UserController(
+    private val userService: UserService
+) {
+    @PostMapping("/signup")
+    fun signUp(
+        @Valid @RequestBody request: SignUpRequest
+    ): ResponseEntity<UserResponse> {
+        return userService.signUp(request.toDto())
+            .toResponse()
+            .wrapOk()
+    }
+
+    @PostMapping("/signin")
+    fun signIn(
+        @Valid @RequestBody request: SignInRequest
+    ): ResponseEntity<Token> {
+        return userService.signIn(request.toDto())
+            .wrapOk()
+    }
+
+}
