b2ca770aeb95bc976801b1c41deabc5ecec82838
dlswns2480 <dlswns661035@gmail.com>
2024-09-16 15:09:46 +0900

    feat : Article 수정 로직 및 테스트

--- core/src/main/kotlin/com/junlog/article/domain/model/Article.kt
+++ core/src/main/kotlin/com/junlog/article/domain/model/Article.kt
@@ -3,6 +3,11 @@ package com.junlog.article.domain.model
 data class Article(
     val id: Long = 0L,
     val userId: Long,
-    val title: String,
-    val content: String
-)
+    var title: String,
+    var content: String
+) {
+    fun edit(title: String, content: String) {
+        this.title = title
+        this.content = content
+    }
+}
